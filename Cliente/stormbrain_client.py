243b9de0fc1dc78102e2758e2a838da6156d4367
Joao Albuquerque <joao.albuquerque.t0131873@edu.atec.pt>
2025-09-28 08:48:12 +0100

    Definiçao das funcoes iniciar servidor e gerir client e iniciar a logica do chat

--- Cliente/stormbrain_client.py
+++ Cliente/stormbrain_client.py
@@ -7,10 +7,28 @@ target_host="127.0.0.1"
 
 clientsocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-clientsocket.connect((target_host, target_port))
 
-print("Conectado ao servidor")
+try:
 
-time.sleep(10)
+    clientsocket.connect((target_host, target_port))
+    
+    print("Conectado ao servidor. Enviar mensagem...")
 
-clientsocket.close()
+    mensagem_enviar=input("Mensagem: ")
+
+    clientsocket.send(mensagem_enviar.encode('utf-8'))
+    print(f"Enviado: {mensagem_enviar}")
+
+    resposta=clientsocket.rcv(1024).decode('utf-8')
+    print(f"Recebido: {resposta}")
+
+    time.sleep(1)
+
+except:
+    print("Erro: servidor inativo ou erro de comunicação")
+
+finally:
+
+    clientsocket.close()
+    print("Conexao fechada")
+    
--- Servidor/stormbrain_servidor.py
+++ Servidor/stormbrain_servidor.py
@@ -2,23 +2,70 @@ import socket
 import threading
 import time
 
+clients=[]
 host="127.0.0.1"
 port=12345
 
-serversocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def gerir_client(clientsocket, clientaddress):
 
-serversocket.bind((host,port)) # colocar em escuta
+    print(f"Nova Conexão: {clientaddress}")
 
-serversocket.listen(5) # estabelecer o limite de conexoes
-print(f"Servidor em escuta")
+    try:
+        while True:
 
-clientsocket, clientaddress=serversocket.accept()
-print(f"Conexao aceite: {clientaddress}")
+            mensagem=clientsocket.recv(1024).decode('utf-8')
 
-time.sleep(5)
+            if not mensagem or mensagem.lower()=="exit":
 
-clientsocket.close()
-serversocket.close()
+                print(f"Desconectado {clientaddress}")
+                break
+
+            print(f"{clientaddress[0]}:{clientaddress[1]} Enviou: {mensagem}")
+
+            clientsocket.send("Mensagem enviada".encode('utf-8'))
+        
+    except:
+        pass
+
+    if clientsocket in clients:
+
+        clients.remove(clientsocket)
+
+    clientsocket.close()
+    
+def iniciar_server():
+
+    serversocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    serversocket.bind((host,port)) # colocar em escuta
+
+    serversocket.listen(5) # estabelecer o limite de conexoes
+    print(f"Servidor em escuta")
+
+    while True:
+
+        try:
+
+            clientsocket, clientaddress=serversocket.accept()
+            print(f"Conexao aceite: {clientaddress}")
+
+            clients.append(clientsocket)
+
+            thread=threading.Thread(target=gerir_client, args=(clientsocket, clientaddress))
+            thread.start() #iniciar uma thread para o client
+
+        except:
+            print("Ocorreu um erro.")
+            break
+
+    time.sleep(5)
+
+    serversocket.close()
+
+
+if __name__ == "__main__":
+    iniciar_server()
+
 
 
 
